bba465ab07031afd0da02e0bc9ebdc1a77584ebd
stephane <stephanesome@gmail.com>
2024-06-24 17:32:19 -0400

    na

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAiO5t7jMzYHSUGcFtx8Y3jIwRPIVsxmrk",
-    authDomain: "add-book24.firebaseapp.com",
-    projectId: "add-book24",
-    storageBucket: "add-book24.appspot.com",
-    messagingSenderId: "334905319705",
-    appId: "1:334905319705:web:fbd5f239e0a891d0daa588",
-    measurementId: "G-05FGSN9VKV"
+    apiKey: "<your-key>",
+    authDomain: "<your-project-authdomain>",
+    projectId: "<your-project-id>",
+    storageBucket: "<your-storage-bucket>",
+    messagingSenderId: "<your-messaging-sender-id>",
+    appId: "<your-app-id>",
+    measurementId: "<your-measurement-id>"
   }
 };
